7a3ce8b2932e7297f1824e46daa85639c4fb22bd
Are <235915091+are4c4@users.noreply.github.com>
2026-09-01 06:17:00 +0900

    Extend domain repository split for migrated stores

--- lib/repositories/domain_repositories.dart
+++ lib/repositories/domain_repositories.dart
@@ -1,6 +1,9 @@
 import '../data/app_database.dart';
+import '../data/bookmark_attachment_store.dart';
 import '../data/bookmark_repository.dart';
+import '../data/pdf_annotation_store.dart';
 import '../data/person_roles.dart';
+import '../data/tag_group_store.dart';
 import '../data/workspace_store.dart';
 import 'backlink_repository.dart';
 import 'full_text_search_repository.dart';
@@ -9,6 +12,7 @@ class WorkspaceRepository {
   const WorkspaceRepository(this._root);
   final BookmarkRepository _root;
 
+  Stream<List<WorkspaceInfo>> watchAll() => _root.workspaceStore.watchWorkspaces();
   Future<List<WorkspaceInfo>> list() => _root.listWorkspaces();
   Future<int> create(String name, {String icon = '📁', int colorValue = 4288585374}) =>
       _root.createWorkspace(name, icon: icon, colorValue: colorValue);
@@ -32,6 +36,21 @@ class TagRepository {
   Future<void> delete(Tag tag) => _root.deleteTag(tag);
   Future<void> setForBookmark(BookmarkItem bookmark, Iterable<Tag> tags) =>
       _root.setBookmarkTagsFromDatabase(bookmark, tags);
+}
+
+class TagGroupRepository {
+  TagGroupRepository(BookmarkRepository root) : _store = TagGroupStore(root.lifecycleStore.database);
+
+  final TagGroupStore _store;
+
+  Stream<List<TagGroupInfo>> watchAll() => _store.watchGroups();
+  Stream<Map<int, int?>> watchAssignments() => _store.watchTagGroupIds();
+  Future<List<TagGroupInfo>> list() => _store.listGroups();
+  Future<int> create(String name) => _store.createGroup(name);
+  Future<void> rename(TagGroupInfo group, String name) => _store.renameGroup(group.id, name);
+  Future<void> delete(TagGroupInfo group) => _store.deleteGroup(group.id);
+  Future<void> assign(Tag tag, TagGroupInfo? group) => _store.setTagGroup(tag.id, group?.id);
+  Future<void> reorder(List<int> ids) => _store.reorderGroups(ids);
 }
 
 class PeopleRepository {
@@ -101,9 +120,63 @@ class CollectionRepository {
   Stream<List<BookmarkItem>> watchBookmarksFor(CollectionRecord collection) =>
       _root.watchBookmarksForCollection(collection);
   Future<int> create(String name, {String? note}) => _root.createCollection(name, note: note);
-  Future<void> delete(CollectionRecord collection) => _root.deleteCollection(collection);
+  Future<void> delete(CollectionRecord collection) => _root.deleteCollection(collection.id);
   Future<void> setForBookmark(BookmarkItem bookmark, Iterable<CollectionRecord> collections) =>
       _root.setBookmarkCollections(bookmark, collections);
+}
+
+class AttachmentRepository {
+  AttachmentRepository(BookmarkRepository root)
+      : _store = BookmarkAttachmentStore(root.lifecycleStore.database);
+
+  final BookmarkAttachmentStore _store;
+
+  Stream<List<BookmarkAttachment>> watchForBookmark(int bookmarkId) =>
+      _store.watchForBookmark(bookmarkId);
+  Future<List<BookmarkAttachment>> listForBookmark(int bookmarkId) =>
+      _store.listForBookmark(bookmarkId);
+  Future<int> add({
+    required int bookmarkId,
+    required String fileName,
+    required String path,
+    required String kind,
+    required int sizeBytes,
+  }) =>
+      _store.add(
+        bookmarkId: bookmarkId,
+        fileName: fileName,
+        path: path,
+        kind: kind,
+        sizeBytes: sizeBytes,
+      );
+  Future<void> remove(int id) => _store.remove(id);
+}
+
+class PdfAnnotationRepository {
+  PdfAnnotationRepository(BookmarkRepository root)
+      : _store = PdfAnnotationStore(root.lifecycleStore.database);
+
+  final PdfAnnotationStore _store;
+
+  Stream<List<PdfAnnotationRecord>> watchForAttachment(int attachmentId) =>
+      _store.watchForAttachment(attachmentId);
+  Future<List<PdfAnnotationRecord>> listForAttachment(int attachmentId) =>
+      _store.listForAttachment(attachmentId);
+  Future<void> add({
+    required int attachmentId,
+    required int pageNumber,
+    required String kind,
+    String selectedText = '',
+    String note = '',
+  }) =>
+      _store.add(
+        attachmentId: attachmentId,
+        pageNumber: pageNumber,
+        kind: kind,
+        selectedText: selectedText,
+        note: note,
+      );
+  Future<void> remove(int id) => _store.remove(id);
 }
 
 class BookmarkLifecycleRepository {
@@ -123,9 +196,12 @@ class AppRepositories {
   AppRepositories._(this.bookmarks)
       : workspaces = WorkspaceRepository(bookmarks),
         tags = TagRepository(bookmarks),
+        tagGroups = TagGroupRepository(bookmarks),
         people = PeopleRepository(bookmarks),
         photos = PhotoRepository(bookmarks),
         collections = CollectionRepository(bookmarks),
+        attachments = AttachmentRepository(bookmarks),
+        pdfAnnotations = PdfAnnotationRepository(bookmarks),
         lifecycle = BookmarkLifecycleRepository(bookmarks),
         search = FullTextSearchRepository(bookmarks),
         backlinks = BacklinkRepository(bookmarks);
@@ -135,9 +211,12 @@ class AppRepositories {
   final BookmarkRepository bookmarks;
   final WorkspaceRepository workspaces;
   final TagRepository tags;
+  final TagGroupRepository tagGroups;
   final PeopleRepository people;
   final PhotoRepository photos;
   final CollectionRepository collections;
+  final AttachmentRepository attachments;
+  final PdfAnnotationRepository pdfAnnotations;
   final BookmarkLifecycleRepository lifecycle;
   final FullTextSearchRepository search;
   final BacklinkRepository backlinks;
