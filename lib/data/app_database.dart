1fa4fb394bc9595663ce373e1cdf1ef7cf9e9255
Are <235915091+are4c4@users.noreply.github.com>
2026-09-02 00:52:01 +0900

    Use split Drift schema part

--- lib/data/app_database.dart
+++ lib/data/app_database.dart
@@ -2,6 +2,7 @@ import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
 
 part 'app_database.g.dart';
+part 'app_database_schema.dart';
 
 class Bookmarks extends Table {
   IntColumn get id => integer().autoIncrement()();
@@ -221,61 +222,6 @@ class PdfAnnotations extends Table {
   TextColumn get selectedText => text().withDefault(const Constant(''))();
   TextColumn get note => text().withDefault(const Constant(''))();
   TextColumn get createdAt => text()();
-}
-
-class BookmarkItem {
-  const BookmarkItem({
-    required this.id,
-    required this.url,
-    required this.title,
-    required this.createdAt,
-    required this.favorite,
-    required this.status,
-    required this.rating,
-    required this.openCount,
-    required this.tags,
-    required this.people,
-    required this.photos,
-    required this.collections,
-    this.readingStatus = 'unread',
-    this.storageState = 'active',
-    this.genre = '',
-    this.deletedAt,
-    this.coverPhoto,
-    this.thumbnail,
-    this.description,
-    this.lastOpenedAt,
-  });
-
-  final int id;
-  final String url;
-  final String title;
-  final String? thumbnail;
-  final String? description;
-  final DateTime createdAt;
-  final bool favorite;
-
-  /// Legacy compatibility status. New code should prefer readingStatus/storageState.
-  final String status;
-  final String readingStatus;
-  final String storageState;
-  final String genre;
-  final DateTime? deletedAt;
-
-  final int rating;
-  final DateTime? lastOpenedAt;
-  final int openCount;
-  final List<Tag> tags;
-  final List<Person> people;
-  final List<PhotoRecord> photos;
-  final List<CollectionRecord> collections;
-  final PhotoRecord? coverPhoto;
-}
-
-class SavedViewConfig {
-  const SavedViewConfig({required this.view, required this.tags});
-  final SavedView view;
-  final List<Tag> tags;
 }
 
 @DriftDatabase(
