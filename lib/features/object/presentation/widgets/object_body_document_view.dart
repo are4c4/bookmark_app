573ab15eb23742d2cbd758f0b89b4c7f3567019c
Are <235915091+are4c4@users.noreply.github.com>
2026-09-03 04:33:50 +0900

    Allow Body document hosts to inject block actions

--- lib/features/object/presentation/widgets/object_body_document_view.dart
+++ lib/features/object/presentation/widgets/object_body_document_view.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 import '../../../../domain/object_body.dart';
+import '../../../../domain/object_body_block_actions.dart';
 import '../../../../domain/object_body_block_presentation.dart';
 import 'object_body_block_view.dart';
+
+typedef ObjectBodyBlockActionsBuilder = Widget Function(
+  BuildContext context,
+  ObjectBodyBlock block,
+  ObjectBodyBlockPosition position,
+);
 
 /// Shared renderer/editor shell for a whole Object Body document.
 ///
@@ -13,21 +20,25 @@ class ObjectBodyDocumentView extends StatelessWidget {
     super.key,
     required this.document,
     this.presenter = const ObjectBodyBlockPresenter(),
+    this.positionResolver = const ObjectBodyBlockPositionResolver(),
     this.onTextChanged,
     this.onChecklistChanged,
     this.onObjectReferenceTap,
     this.onDatabaseViewTap,
     this.onAssetTap,
+    this.blockActionsBuilder,
     this.emptyBuilder,
   });
 
   final ObjectBodyDocument document;
   final ObjectBodyBlockPresenter presenter;
+  final ObjectBodyBlockPositionResolver positionResolver;
   final void Function(ObjectBodyBlock block, String text)? onTextChanged;
   final void Function(ObjectBodyBlock block, bool checked)? onChecklistChanged;
   final ValueChanged<ObjectBodyBlock>? onObjectReferenceTap;
   final ValueChanged<ObjectBodyBlock>? onDatabaseViewTap;
   final ValueChanged<ObjectBodyBlock>? onAssetTap;
+  final ObjectBodyBlockActionsBuilder? blockActionsBuilder;
   final WidgetBuilder? emptyBuilder;
 
   @override
@@ -42,25 +53,37 @@ class ObjectBodyDocumentView extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         for (final presentation in presentations)
-          ObjectBodyBlockView(
-            key: ValueKey('object-body-block-${presentation.block.id}'),
-            presentation: presentation,
-            onTextChanged: onTextChanged == null
-                ? null
-                : (text) => onTextChanged!(presentation.block, text),
-            onChecklistChanged: onChecklistChanged == null
-                ? null
-                : (checked) =>
-                    onChecklistChanged!(presentation.block, checked),
-            onObjectReferenceTap: onObjectReferenceTap == null
-                ? null
-                : () => onObjectReferenceTap!(presentation.block),
-            onDatabaseViewTap: onDatabaseViewTap == null
-                ? null
-                : () => onDatabaseViewTap!(presentation.block),
-            onAssetTap: onAssetTap == null
-                ? null
-                : () => onAssetTap!(presentation.block),
+          Column(
+            key: ValueKey('object-body-entry-${presentation.block.id}'),
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ObjectBodyBlockView(
+                key: ValueKey('object-body-block-${presentation.block.id}'),
+                presentation: presentation,
+                onTextChanged: onTextChanged == null
+                    ? null
+                    : (text) => onTextChanged!(presentation.block, text),
+                onChecklistChanged: onChecklistChanged == null
+                    ? null
+                    : (checked) =>
+                        onChecklistChanged!(presentation.block, checked),
+                onObjectReferenceTap: onObjectReferenceTap == null
+                    ? null
+                    : () => onObjectReferenceTap!(presentation.block),
+                onDatabaseViewTap: onDatabaseViewTap == null
+                    ? null
+                    : () => onDatabaseViewTap!(presentation.block),
+                onAssetTap: onAssetTap == null
+                    ? null
+                    : () => onAssetTap!(presentation.block),
+              ),
+              if (blockActionsBuilder != null)
+                blockActionsBuilder!(
+                  context,
+                  presentation.block,
+                  positionResolver.resolve(document, presentation.block.id),
+                ),
+            ],
           ),
       ],
     );
