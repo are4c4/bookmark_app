8c8fdbc708005ec25d6449749e0f96708b8db053
Are <235915091+are4c4@users.noreply.github.com>
2026-09-07 07:07:09 +0900

    feat: add Vault lifecycle inherited scope

---
+++ lib/services/vault_lifecycle_scope.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/widgets.dart';
+
+import 'profile_manager.dart';
+
+class VaultLifecycleScope extends InheritedWidget {
+  const VaultLifecycleScope({
+    super.key,
+    required this.profileState,
+    required this.createVault,
+    required this.openVault,
+    required this.switchVault,
+    required super.child,
+  });
+
+  final ProfileState profileState;
+  final Future<void> Function() createVault;
+  final Future<void> Function() openVault;
+  final Future<void> Function(DatabaseProfile profile) switchVault;
+
+  static VaultLifecycleScope? maybeOf(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<VaultLifecycleScope>();
+
+  static VaultLifecycleScope of(BuildContext context) {
+    final scope = maybeOf(context);
+    assert(scope != null, 'No VaultLifecycleScope found in context.');
+    return scope!;
+  }
+
+  @override
+  bool updateShouldNotify(VaultLifecycleScope oldWidget) =>
+      profileState != oldWidget.profileState ||
+      createVault != oldWidget.createVault ||
+      openVault != oldWidget.openVault ||
+      switchVault != oldWidget.switchVault;
+}
