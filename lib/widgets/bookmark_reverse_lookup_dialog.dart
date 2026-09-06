f59157bba82d5e6344428e0b29895aad60ab3147
Are <235915091+are4c4@users.noreply.github.com>
2026-09-06 15:11:25 +0900

    Route reverse lookup URL composition through shared factory

--- lib/widgets/bookmark_reverse_lookup_dialog.dart
+++ lib/widgets/bookmark_reverse_lookup_dialog.dart
@@ -3,6 +3,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 import '../data/app_database.dart';
 import '../data/bookmark_repository.dart';
+import '../services/bookmark_presentation_resolver_factory.dart';
 import '../services/bookmark_url_resolver.dart';
 import 'bookmark_visual_image.dart';
 
@@ -13,11 +14,8 @@ Future<void> showBookmarkReverseLookupDialog({
   required Stream<List<BookmarkItem>> bookmarks,
   BookmarkUrlResolve? resolveUrl,
 }) {
-  final resolver = resolveUrl ??
-      BookmarkUrlResolver(
-        database: repository.workspaceStore.database,
-        workspaceId: repository.workspaceId,
-      ).resolve;
+  final resolver =
+      resolveUrl ?? BookmarkPresentationResolverFactory.urlFor(repository);
   return showDialog<void>(
     context: context,
     builder: (dialogContext) => AlertDialog(
