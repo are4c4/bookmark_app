42bee7c999d6b99b4b27660c78cc27a10558c1ab
Are <235915091+are4c4@users.noreply.github.com>
2026-09-07 09:31:00 +0900

    test: inject date-valid Daily Note race winner

--- test/daily_note_claim_race_test.dart
+++ test/daily_note_claim_race_test.dart
@@ -107,7 +107,7 @@ void main() {
     await objectStore.setPropertyValue(
       objectId: winnerId,
       property: definition.dateProperty,
-      value: '2026-09-09',
+      value: '2026-09-10',
     );
     await defaultsStore.writeBodyTemplate(
       objectTypeId: definition.objectType.id,
@@ -129,6 +129,10 @@ void main() {
       WHEN NEW.database_id = ${definition.objectType.id}
         AND NEW.id <> $winnerId
       BEGIN
+        UPDATE generic_values
+        SET value_json = '"2026-09-09"'
+        WHERE record_id = $winnerId
+          AND property_id = ${definition.dateProperty.id};
         INSERT OR IGNORE INTO daily_note_registry(workspace_id, note_date, object_id)
         VALUES ($workspaceId, '2026-09-09', $winnerId);
       END
