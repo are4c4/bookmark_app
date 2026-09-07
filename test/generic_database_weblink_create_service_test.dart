f533479d68e24f49163435f05151007ecd65d28a
Are <235915091+are4c4@users.noreply.github.com>
2026-09-07 16:59:54 +0900

    Test managed File creation mode

--- test/generic_database_weblink_create_service_test.dart
+++ test/generic_database_weblink_create_service_test.dart
@@ -3,6 +3,7 @@ import 'package:bookmark_app/data/bidirectional_relation_store.dart';
 import 'package:bookmark_app/data/database_collection_resolver.dart';
 import 'package:bookmark_app/data/database_collection_store.dart';
 import 'package:bookmark_app/data/daily_note_service.dart';
+import 'package:bookmark_app/data/file_object_service.dart';
 import 'package:bookmark_app/data/generic_database_collection_page_data.dart';
 import 'package:bookmark_app/data/generic_database_object_create_service.dart';
 import 'package:bookmark_app/data/generic_database_store.dart';
@@ -89,6 +90,10 @@ void main() {
       systemObjects: systemObjects,
       defaultsStore: defaultsStore,
     ).ensureDefinition(workspaceId);
+    final fileDefinition = await FileObjectService(
+      systemObjects: systemObjects,
+      defaultsStore: defaultsStore,
+    ).ensureDefinition(workspaceId);
     final dailyDefinition = await DailyNoteService(
       genericStore: genericStore,
       objectStore: objectStore,
@@ -107,6 +112,10 @@ void main() {
     expect(
       await service.createModeForObjectType(imageDefinition.objectType.id),
       GenericDatabaseCreateMode.managedImage,
+    );
+    expect(
+      await service.createModeForObjectType(fileDefinition.objectType.id),
+      GenericDatabaseCreateMode.managedFile,
     );
     expect(
       await service.createModeForObjectType(dailyDefinition.objectType.id),
