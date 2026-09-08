8cb912167c135c36d862b49db5190117b869c69f
Are <235915091+are4c4@users.noreply.github.com>
2026-09-08 22:36:32 +0900

    Format Photo retirement regression

--- test/photo_management_page_retirement_test.dart
+++ test/photo_management_page_retirement_test.dart
@@ -27,8 +27,8 @@ void main() {
   });
 
   test('legacy Photo metadata update APIs stay retired', () {
-    final repository =
-        File('lib/data/bookmark_repository.dart').readAsStringSync();
+    final repository = File('lib/data/bookmark_repository.dart')
+        .readAsStringSync();
     final database = File('lib/data/app_database.dart').readAsStringSync();
 
     expect(repository, isNot(contains('updatePhoto(')));
