f107933a439616e0689e0fdadadd3900ae154a18
Are <235915091+are4c4@users.noreply.github.com>
2026-09-11 12:19:31 +0900

    Database UX: apply Dart formatter to Board create service

--- lib/data/object_board_create_service.dart
+++ lib/data/object_board_create_service.dart
@@ -66,10 +66,8 @@ class ObjectBoardCreateService {
     required ObjectGroupBucket<AppObject> targetGroup,
   }) {
     return createWithObjectFactory(
-      createObject: () => _objectStore.createObject(
-        objectTypeId: objectTypeId,
-        title: title,
-      ),
+      createObject: () =>
+          _objectStore.createObject(objectTypeId: objectTypeId, title: title),
       groupProperty: groupProperty,
       targetGroup: targetGroup,
     );
