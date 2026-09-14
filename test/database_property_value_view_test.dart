bf16a3a563e853beb086c0c7b123a933001dfa01
Are <235915091+are4c4@users.noreply.github.com>
2026-09-14 16:23:05 +0900

    C: apply Dart format to hidden-property regression (#1456)

--- test/database_property_value_view_test.dart
+++ test/database_property_value_view_test.dart
@@ -6,8 +6,7 @@ import 'package:flutter_test/flutter_test.dart';
 GenericPropertyRecord property(
   String type, {
   Map<String, dynamic> config = const <String, dynamic>{},
-}) =>
-    GenericPropertyRecord(
+}) => GenericPropertyRecord(
       id: 7,
       databaseId: 3,
       name: type,
