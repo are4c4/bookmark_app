7b3666264ec7d2ee44f4a19ff592c253cb0f4e88
Are <235915091+are4c4@users.noreply.github.com>
2026-09-19 00:50:13 +0900

    Style: format private Bookmark-People helper (#1505)

--- lib/data/app_database.dart
+++ lib/data/app_database.dart
@@ -230,8 +230,13 @@ class AppDatabase extends _$AppDatabase {
     }
   }
 
-  Future<void> _setBookmarkPeople(int bookmarkId, Iterable<String> names) async {
-    await (delete(bookmarkPeople)..where((bp) => bp.bookmarkId.equals(bookmarkId))).go();
+  Future<void> _setBookmarkPeople(
+    int bookmarkId,
+    Iterable<String> names,
+  ) async {
+    await (delete(
+      bookmarkPeople,
+    )..where((bp) => bp.bookmarkId.equals(bookmarkId))).go();
     for (final name in _normalizeNames(names)) {
       final personId = await _ensurePerson(name);
       await into(bookmarkPeople).insert(
